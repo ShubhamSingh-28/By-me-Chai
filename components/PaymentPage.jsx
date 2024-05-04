7bcb0fc1c543e4c04e6732d7570a73cb6ccdc1ec
Shubham <ss9971768704@gmail.com>
2024-05-04 13:02:19 +0530

    done

--- components/PaymentPage.jsx
+++ components/PaymentPage.jsx
@@ -27,7 +27,7 @@ let orderId = a.id
     "description": "Test Transaction",
     "image": "https://example.com/your_logo",
     "order_id": orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-    "callback_url": `/`,
+    "callback_url": `/dashboard`,
     "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
         "name": "Gaurav Kumar", //your customer's name
         "email": "gaurav.kumar@example.com",
